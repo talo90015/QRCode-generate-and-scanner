9aa173315480238f60ff289f996cf38dc1e2424b
Talo <talo90015@gmail.com>
2021-05-03 01:05:44 +0800

    add Toolbar and ready viewPager

--- app/src/main/java/com/talo/theqrcode/MainActivity.kt
+++ app/src/main/java/com/talo/theqrcode/MainActivity.kt
@@ -2,10 +2,33 @@ package com.talo.theqrcode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.talo.theqrcode.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.toolbar.inflateMenu(R.menu.menu_main)
+        binding.toolbar.setOnMenuItemClickListener {
+            when(it.itemId){
+                R.id.qr_generate ->{
+
+                }
+                R.id.qr_scanner ->{
+
+                }
+            }
+            false
+        }
     }
+
+
 }
